fb0947326528e7351a0a2984120d2291e9b0e48d
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-12 19:38:12 -0800

    initial setup, started start up, added connection string and folders

---
+++ InstaFit/Data/InstaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaFit.Data
+{
+    public class InstaDbContext : DbContext
+    {
+       public InstaDbContext(DbContextOptions<InstaDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ InstaFit/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstaFit.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InstaFit
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<InstaDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+
+
+
+
+
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
