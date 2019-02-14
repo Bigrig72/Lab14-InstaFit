1794365984a47d02b9a3e60eca878875adae76b6
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-14 10:04:22 -0800

    added footer with nav links

--- InstaFit/Models/Services/FitMangementService.cs
+++ InstaFit/Models/Services/FitMangementService.cs
@@ -1,9 +1,7 @@
 ﻿using InstaFit.Data;
 using InstaFit.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace InstaFit.Models.Services
---
+++ InstaFit/Pages/FitnessPosts/Manage.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstaFit.Models;
+using InstaFit.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InstaFit.Pages.FitnessPosts
+{
+    public class ManageModel : PageModel
+    {
+        private readonly IFit _fitnessPost;
+
+        [FromRoute]
+        public int? ID { get; set; }
+
+        [BindProperty]
+        public FitnessPost FitnessPost { get; set; }
+
+        public ManageModel(IFit fitnessPost)
+        {
+            _fitnessPost = fitnessPost;
+        }
+
+
+        public async Task OnGet()
+        {
+            FitnessPost = await _fitnessPost.FindFitnessPost(ID.GetValueOrDefault()) ?? new FitnessPost();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            var post = await _fitnessPost.FindFitnessPost(ID.GetValueOrDefault()) ?? new FitnessPost();
+
+            post.Description = FitnessPost.Description;
+            post.Location = FitnessPost.Location;
+
+            await _fitnessPost.SaveAsync(post);
+
+            return RedirectToPage("/FitnessPosts/Index", new { id = post.ID });
+        }
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await _fitnessPost.DeleteAsync(ID.Value);
+            return RedirectToPage("/Index");
+        }
+    }
+}
